daa792244ed5bbf951b2096314b549fff969e6d4
e.qorri <e.qorri@teamsystem.com>
2024-08-23 20:17:01 +0200

    continue to fix the menu in mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,14 @@
 import React, { useRef } from "react";
 import { Container } from "reactstrap";
 import { HashLink as Link } from "react-router-hash-link";
+import {
+  FaHome,
+  FaInfo,
+  FaWrench,
+  FaCar,
+  FaBlog,
+  FaEnvelope,
+} from "react-icons/fa";
 import "../../styles/header.css";
 import logo from "../../assets/all-images/marlon.jpeg";
 
@@ -8,26 +16,32 @@ const navLinks = [
   {
     path: "/",
     display: "Home",
+    icon: <FaHome size={25} />,
   },
   {
     path: "/#about",
     display: "About",
+    icon: <FaInfo size={25} />,
   },
   {
     path: "/#services",
     display: "Services",
+    icon: <FaWrench size={25} />,
   },
   {
     path: "/#cars",
     display: "Cars",
+    icon: <FaCar size={25} />,
   },
   {
     path: "/#blogs",
     display: "Blog",
+    icon: <FaBlog size={25} />,
   },
   {
     path: "/#contact",
     display: "Contact",
+    icon: <FaEnvelope size={25} />,
   },
 ];
 
@@ -46,14 +60,17 @@ const Header = () => {
               src={logo}
             />
             <div className="navigation__wrapper">
-              <span className="mobile__menu">
-                <i class="ri-menu-line" onClick={toggleMenu}></i>
+              <span className="mobile__menu" onClick={toggleMenu}>
+                <i class="ri-menu-line"></i>
               </span>
-
-              <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+              <div className="navigation" ref={menuRef}>
+                <span className="close__menu" onClick={toggleMenu}>
+                  <i class="ri-close-line"></i>
+                </span>
                 <div className="menu">
                   {navLinks.map((item, index) => (
                     <Link to={item.path} className="nav__item" key={index}>
+                      {item.icon}
                       {item.display}
                     </Link>
                   ))}
