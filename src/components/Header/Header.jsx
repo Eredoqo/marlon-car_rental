1e4353f33d4f9af4608918ce112bad8f67083fe7
e.qorri <e.qorri@teamsystem.com>
2024-08-27 20:42:07 +0200

    fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Container } from "reactstrap";
 import { HashLink as Link } from "react-router-hash-link";
 import {
@@ -16,39 +16,55 @@ const navLinks = [
   {
     path: "/",
     display: "Home",
-    icon: <FaHome size={25} />,
+    icon: <FaHome size={30} />,
   },
   {
     path: "/#about",
     display: "About",
-    icon: <FaInfo size={25} />,
+    icon: <FaInfo size={30} />,
   },
   {
     path: "/#services",
     display: "Services",
-    icon: <FaWrench size={25} />,
+    icon: <FaWrench size={30} />,
   },
   {
     path: "/#cars",
     display: "Cars",
-    icon: <FaCar size={25} />,
+    icon: <FaCar size={30} />,
   },
   {
     path: "/#blogs",
     display: "Blog",
-    icon: <FaBlog size={25} />,
+    icon: <FaBlog size={30} />,
   },
   {
     path: "/#contact",
     display: "Contact",
-    icon: <FaEnvelope size={25} />,
+    icon: <FaEnvelope size={30} />,
   },
 ];
 
 const Header = () => {
   const menuRef = useRef(null);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const toggleMenu = () => menuRef.current.classList.toggle("menu__active");
+
+  const closeMenu = () => {
+    if (menuRef.current.classList.contains("menu__active")) {
+      menuRef.current.classList.remove("menu__active");
+    }
+  };
 
   return (
     <header className="header">
@@ -60,22 +76,48 @@ const Header = () => {
               src={logo}
             />
             <div className="navigation__wrapper">
-              <span className="mobile__menu" onClick={toggleMenu}>
-                <i class="ri-menu-line"></i>
-              </span>
-              <div className="navigation" ref={menuRef}>
-                <span className="close__menu" onClick={toggleMenu}>
-                  <i class="ri-close-line"></i>
-                </span>
+              {isMobile ? (
+                <>
+                  <span className="mobile__menu" onClick={toggleMenu}>
+                    <i className="ri-menu-line"></i>
+                  </span>
+                  <div className="navigation" ref={menuRef}>
+                    <span className="close__menu" onClick={toggleMenu}>
+                      <i className="ri-close-line"></i>
+                    </span>
+
+                    <div className="menu">
+                      {navLinks.map((item, index) => (
+                        <div
+                          key={index}
+                          style={{
+                            display: "flex",
+                            flexDirection: "row",
+                          }}
+                        >
+                          <span className="icon">{item.icon}</span>
+                          <Link
+                            to={item.path}
+                            className="nav__item__mobile"
+                            key={index}
+                            onClick={closeMenu}
+                          >
+                            <span>{item.display}</span>
+                          </Link>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </>
+              ) : (
                 <div className="menu">
                   {navLinks.map((item, index) => (
                     <Link to={item.path} className="nav__item" key={index}>
-                      {item.icon}
                       {item.display}
                     </Link>
                   ))}
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </Container>
